0b891dbad2f462b7eaeee47bfbd87a0893236199
David Lawrence Miller <github@ninepointeightone.net>
2021-05-29 15:55:15 +0100

    working with twitter stuff via R

---
+++ omgno.R
@@ -0,0 +1,27 @@
+# David Lawrence Miller 2021
+
+make_im <- system2("./lex.sh")
+
+post_it <- function(fn="merged_final.png"){
+  # switch account
+  fuzzy_dog0 <- system2("/usr/local/bin/twurl", "set default LexKnowsSystems")
+  # how to from here:
+  # https://github.com/twitter/twurl/issues/58
+  fuzzy_dog <- system2("/usr/local/bin/twurl",
+                  paste0("-H upload.twitter.com \"/1.1/media/upload.json\" -f ",
+                         fn,
+                         " -F media -X POST"), stdout=TRUE)
+
+  fuzzy_dog <- sub(".*\"media_id\":(\\d+).*", "\\1", fuzzy_dog)
+  cat(fuzzy_dog,"\n")
+  fuzzy_dog2 <- system2("/usr/local/bin/twurl",
+                        paste0("\"/1.1/statuses/update.json\" -d \"media_ids=",
+                               fuzzy_dog,
+                               "&status=\""))
+
+  # reset default
+  fuzzy_dog0 <- system2("/usr/local/bin/twurl", "set default goodsdmbot")
+}
+
+post_it()
+
